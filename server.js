c0fbe84aebc008750fbc8612587cf38ebbc66df8
Matthew Rumschlag <mrumschlag@gmail.com>
2019-01-16 18:10:39 -0500

    update

--- server.js
+++ server.js
@@ -6,31 +6,8 @@ require('dotenv').config()
 app.set('port', (process.env.PORT || 5000));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/dist');
-app.get('/:filename', function(request, response) {
-     var env = process.env.APP_ENV;
-  if (env == 'staging') {
-    var envName = 'staging'
-  } else if (env == 'production') {
-    var envName = 'production'
-  } else {
-    var envName = 'review app'
-  }
-	 console.log(filename);	 
-	 response.render(filename, {env: envName});
-});
-	
-app.get('/', function(request, response) {
-  var env = process.env.APP_ENV;
-  if (env == 'staging') {
-    var envName = 'staging'
-  } else if (env == 'production') {
-    var envName = 'production'
-  } else {
-    var envName = 'review app'
-  }
-  response.render('index.html', { env: envName});
-});
+app.use(__dirname + '/dist');
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app running at localhost:" + app.get('port'));

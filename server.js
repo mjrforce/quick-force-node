6b142b018cff28301d9e8df3f81f1e2986b9d37a
Matthew Rumschlag <mrumschlag@gmail.com>
2019-01-16 18:12:55 -0500

    update

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var app = express();
 
-require('dotenv').config()
-
 app.set('port', (process.env.PORT || 5000));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
 app.use(__dirname + '/dist');
-
 
 app.listen(app.get('port'), function() {
   console.log("Node app running at localhost:" + app.get('port'));
